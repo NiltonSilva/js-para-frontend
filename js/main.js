a8a4b3f62ce179fd8de28b3d33ba08ddb085621d
Nilton S. Ferreira <nsfgeo@hotmail.com>
2022-08-29 12:53:59 -0300

    continuar da aula 6 (funcoes anonimas)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
-}
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-// Se eu chamar a função com (), ou seja, tocaSomPom(), o som será tocado automaticamente, e isso ocasionará um erro. Do jeito que está agora o som fica guardado e será executado somente quando eu acionar o botão com o click do mouse.
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+/*
+   document.querySelector('.tecla_pom').onclick = tocaSomPom;
+    
+   Se eu chamar a função com (), ou seja, tocaSomPom(), o som será tocado automaticamente, e isso ocasionará um erro. Do jeito que está agora o som fica guardado e será executado somente quando eu acionar o botão com o click do mouse.
+
+*/
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = tocaSom;
+    contador += 1;
+    console.log(contador)
+}
