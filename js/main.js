67541b33b3c8ec708be1cb62cde735631b7c9992
Nilton S. Ferreira <nsfgeo@hotmail.com>
2022-08-30 14:45:05 -0300

    concluido configuração para pagar tecla Enter e Barra de Espaço

--- js/main.js
+++ js/main.js
@@ -4,23 +4,31 @@ function tocaSom(idElementoAudio) {
 }
 /*
    document.querySelector('.tecla_pom').onclick = tocaSomPom;
-    
    Se eu chamar a função com (), ou seja, tocaSomPom(), o som será tocado automaticamente, e isso ocasionará um erro. Do jeito que está agora o som fica guardado e será executado somente quando eu acionar o botão com o click do mouse.
-
 */
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while(contador < listaDeTeclas.length){
-
-    let tecla = listaDeTeclas[contador];
+for(let i = 0; i < listaDeTeclas.length; i++){
+    let tecla = listaDeTeclas[i];
     let instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
+
     tecla.onclick = function(){
         tocaSom(idAudio);
     }
-    contador += 1;
+
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter' || evento.code === 'NumpadEnter'){
+            tecla.classList.add('ativa');
+            
+        }
+        console.log(evento)
+    }
+    
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
+
+
