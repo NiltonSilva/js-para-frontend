d7f55d8a4451dbb840a2eac58b43a245e8871258
Nílton Silva Ferreira <nsfgeo@hotmail.com>
2022-08-29 23:25:28 -0300

    conclusao do exercicio

--- js/main.js
+++ js/main.js
@@ -14,7 +14,13 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSom;
+
+    let tecla = listaDeTeclas[contador];
+    let instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
     contador += 1;
-    console.log(contador)
 }
